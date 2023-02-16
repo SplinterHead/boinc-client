cf8bb521cb079b9dc9c5b781cd8c89c0be1f7f6d
Lewis England <lewis2004@hotmail.com>
2023-02-16 10:49:28 +0000

    fix: send binary bool in active_only for get_results
    Relates-to: #18

--- src/pyboinc/results.py
+++ src/pyboinc/results.py
@@ -5,12 +5,9 @@ from pyboinc.clients.rpc_client import RpcClient
 
 def results(client: RpcClient, active_only: bool = False) -> dict:
     """Show tasks."""
-    if active_only:
-        rpc_resp = client.make_request(
-            "<get_results><active_only></active_only></get_results>"
-        )
-    else:
-        rpc_resp = client.make_request("<get_results/>")
+    rpc_resp = client.make_request(
+        f"<get_results><active_only>{int(active_only)}</active_only></get_results>"
+    )
     rpc_json = xmltodict.parse(rpc_resp, force_list="result")
     return {"results": [r for r in rpc_json["results"]["result"]]}
 
--- tests/results/test_results.py
+++ tests/results/test_results.py
@@ -36,7 +36,7 @@ def test_can_get_only_active_results(
     )
     _ = results(client=mock_rpc_client, active_only=True) == multi_result_dict
     m_call.assert_called_once_with(
-        "<get_results><active_only></active_only></get_results>"
+        "<get_results><active_only>1</active_only></get_results>"
     )
 
 
