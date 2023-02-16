fe9bb7393d5ed8cbfd8c3ff294e5c79ed7dedb39
Lewis England <lewis2004@hotmail.com>
2023-02-16 10:33:12 +0000

    test: add integration tests for messages, projects and results
    Relates-to: #4

--- tests/integration/test_pyboinc.py
+++ tests/integration/test_pyboinc.py
@@ -1,6 +1,52 @@
 from pytest import mark
 
 
+# Messages
+@mark.integration
+def test_messages(boinc_client):
+    result = boinc_client.get_messages()
+    assert result
+    assert "messages" in result
+
+
+@mark.integration
+def test_message_with_start(boinc_client):
+    all_msg = boinc_client.get_messages()
+    result = boinc_client.get_messages(start=1)
+    assert result
+    assert "messages" in result
+    assert len(result["messages"]) == len(all_msg["messages"]) - 1
+
+
+@mark.integration
+def test_message_count(boinc_client):
+    result = boinc_client.get_message_count()
+    assert result
+    assert "message_count" in result
+
+
+@mark.integration
+def test_public_notices(boinc_client):
+    result = boinc_client.get_public_notices()
+    assert result
+    assert "notices" in result
+
+
+# Projects
+@mark.integration
+def test_all_projects_list(boinc_client):
+    result = boinc_client.get_all_projects()
+    assert result
+    assert "projects" in result
+
+
+# Results
+# @mark.integration
+# def test_results(boinc_client):
+#     result = boinc_client.get_results()
+#     assert result
+
+
 # Versions
 @mark.integration
 def test_container_version(boinc_client):
@@ -13,7 +59,7 @@ def test_container_version(boinc_client):
 def test_container_update(boinc_client):
     result = boinc_client.get_client_update()
     assert result
-    assert result is not {}
+    assert "update" in result
 
 
 # Messages
