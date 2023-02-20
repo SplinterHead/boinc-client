c1208d3d84fae6ca04b5972b67ec54b79d30d0c9
Lewis England <lewis2004@hotmail.com>
2023-02-20 16:43:23 +0000

    feat: Create marshmallow schema for the Project List
    Update the tests to use XML files for accurate testing
    
    Relates-to: #23

---
+++ src/boinc_client/models/project_list.py
@@ -0,0 +1,38 @@
+from marshmallow import Schema, fields, pre_load
+
+
+class ProjectPlatform(Schema):
+    name = fields.Str()
+
+
+class Project(Schema):
+    name = fields.Str()
+    id = fields.Int()
+    url = fields.Url()
+    web_url = fields.Url()
+    general_area = fields.Str()
+    specific_area = fields.Str()
+    description = fields.Str()
+    home = fields.Str()
+    platforms = fields.List(fields.Nested(ProjectPlatform()))
+    image = fields.Str()
+    summary = fields.Str()
+    keywords = fields.Str()
+
+    @pre_load
+    def _convert_platform_dict_to_list(self, data, **kwargs):
+        data["platforms"] = (
+            [{"name": d} for d in data["platforms"]["name"]]
+            if type(data["platforms"]["name"]) == list
+            else [data["platforms"]]
+        )
+        return data
+
+
+class ProjectList(Schema):
+    projects = fields.List(fields.Nested(Project()))
+
+    @pre_load
+    def _convert_none_to_empty_dict(self, data, **kwargs):
+        data["projects"] = data["projects"]["project"] if data["projects"] else []
+        return data
