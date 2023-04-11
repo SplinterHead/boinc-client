9ac65afd61a6d0cb13b08202c2c04717c1531578
Lewis England <lewis2004@hotmail.com>
2023-04-11 13:20:19 +0100

    refactor: switch empty string check in projects.py

--- src/boinc_client/projects.py
+++ src/boinc_client/projects.py
@@ -11,7 +11,7 @@ EMPTY_PROJECT_LIST = "<projects></projects>"
 def all_projects(client: RpcClient) -> dict:
     """Used to get a list of all the projects as found in the all_projects_list.xml file."""
     rpc_resp = client.make_request("<get_all_projects_list/>")
-    if rpc_resp == "":
+    if not rpc_resp:
         rpc_resp = EMPTY_PROJECT_LIST
     rpc_json = xmltodict.parse(rpc_resp, force_list="project")
     return Projects().load(rpc_json)
