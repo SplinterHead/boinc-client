a734f7f683dab98e05e1639ef8e04639a6e6035d
Lewis England <lewis2004@hotmail.com>
2024-03-06 13:49:27 +0000

    feat: add ability to set the GPU run mode
    relates-to: #74

--- src/boinc_client/modes.py
+++ src/boinc_client/modes.py
@@ -16,3 +16,17 @@ def set_cpu_run_mode(client: RpcClient, run_mode: str, duration: int = 0):
     return {
         "error": "Invalid CPU mode given. Select from: always, never, auto or restore"
     }
+
+
+def set_gpu_run_mode(client: RpcClient, run_mode: str, duration: int = 0):
+    if run_mode in ["always", "never", "auto", "restore"]:
+        req_string = f"<set_gpu_mode><{run_mode}/>"
+        if duration > 0:
+            req_string += f"<duration>{duration}</duration>"
+        req_string += "</set_gpu_mode>"
+        rpc_resp = client.make_request(req_string)
+        rpc_json = xmltodict.parse(rpc_resp)
+        return GenericResponse().load(rpc_json)
+    return {
+        "error": "Invalid GPU mode given. Select from: always, never, auto or restore"
+    }
--- tests/modes/test_modes.py
+++ tests/modes/test_modes.py
@@ -1,4 +1,5 @@
 from boinc_client import set_cpu_run_mode
+from boinc_client.modes import set_gpu_run_mode
 
 
 def test_can_set_cpu_run_mode(mocker, mock_rpc_client):
@@ -32,5 +33,40 @@ def test_cannot_set_invalid_cpu_run_mode(mocker, mock_rpc_client):
     )
     assert set_cpu_run_mode(client=mock_rpc_client, run_mode="dummy") == {
         "error": "Invalid CPU mode given. Select from: always, never, auto or restore"
+    }
+    assert not m.called
+
+
+def test_can_set_gpu_run_mode(mocker, mock_rpc_client):
+    m = mocker.patch(
+        "boinc_client.clients.rpc_client.RpcClient.make_request",
+        return_value="<success/>",
+    )
+    assert set_gpu_run_mode(client=mock_rpc_client, run_mode="always") == {
+        "success": True
+    }
+    m.assert_called_with("<set_gpu_mode><always/></set_gpu_mode>")
+
+
+def test_can_set_gpu_run_mode_with_duration(mocker, mock_rpc_client):
+    m = mocker.patch(
+        "boinc_client.clients.rpc_client.RpcClient.make_request",
+        return_value="<success/>",
+    )
+    assert set_gpu_run_mode(client=mock_rpc_client, run_mode="always", duration=60) == {
+        "success": True
+    }
+    m.assert_called_with(
+        "<set_gpu_mode><always/><duration>60</duration></set_gpu_mode>"
+    )
+
+
+def test_cannot_set_invalid_gpu_run_mode(mocker, mock_rpc_client):
+    m = mocker.patch(
+        "boinc_client.clients.rpc_client.RpcClient.make_request",
+        return_value="<error/>",
+    )
+    assert set_gpu_run_mode(client=mock_rpc_client, run_mode="dummy") == {
+        "error": "Invalid GPU mode given. Select from: always, never, auto or restore"
     }
     assert not m.called
