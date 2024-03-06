8ae67f9b93c72caf7ab6ea9a9b7ff6d93d55347d
Lewis England <lewis2004@hotmail.com>
2024-03-06 13:54:32 +0000

    fix: expose set_gpu_run_mode to the boinc_client
    allow setting the duration from the boinc_client too
    
    relates-to: #74

--- src/boinc_client/boinc_client.py
+++ src/boinc_client/boinc_client.py
@@ -1,7 +1,7 @@
 from boinc_client.clients.rpc_client import RpcClient
 
 from .messages import message_count, messages, public_notices
-from .modes import set_cpu_run_mode
+from .modes import set_cpu_run_mode, set_gpu_run_mode
 from .preferences import (
     get_global_prefs_file,
     get_global_prefs_override,
@@ -162,5 +162,8 @@ class Boinc:
     def read_global_prefs_override(self) -> dict:
         return read_global_prefs_override(self.rpc_client)
 
-    def set_cpu_run_mode(self, run_mode: str) -> dict:
-        return set_cpu_run_mode(self.rpc_client, run_mode)
+    def set_cpu_run_mode(self, run_mode: str, duration: int = 0) -> dict:
+        return set_cpu_run_mode(self.rpc_client, run_mode, duration)
+
+    def set_gpu_run_mode(self, run_mode: str, duration: int = 0) -> dict:
+        return set_gpu_run_mode(self.rpc_client, run_mode, duration)
